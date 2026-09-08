0be017b018f87efd7a2eaf50aab176cf484c1197
syxiaoji112 <101303390+usbog232@users.noreply.github.com>
2026-09-09 04:41:37 +0800

    更换 App 图标: 蓝色播放器+字幕卡片+翻译箭头 (v0.2.1)

--- script/build_and_run.sh
+++ script/build_and_run.sh
@@ -65,7 +65,7 @@ cat >"$INFO_PLIST" <<PLIST
   <key>CFBundlePackageType</key>
   <string>APPL</string>
   <key>CFBundleShortVersionString</key>
-  <string>0.2.0</string>
+  <string>0.2.1</string>
   <key>CFBundleVersion</key>
   <string>1</string>
   <key>LSMinimumSystemVersion</key>
